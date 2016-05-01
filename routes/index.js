79cd51f4f7afc5eabef58608f14f7878d3af0462
Sai Num Town <sainumtown@gmail.com>
2016-05-01 09:38:37 +0630

    node blog finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mongo =  require('mongodb');
+var db = require('monk')('127.0.0.1:27017/nodeblog');
 
-/* GET home page. */
+
+/* GET home page blog spot. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var db = req.db;
+	var posts = db.get('posts');
+	posts.find({},{},function(err,posts){
+		res.render('index',{posts:posts});	
+	});
+	// res.render('index',{title:"GOod"});
 });
 
 module.exports = router;
